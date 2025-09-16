3fc26fc60ff780feffe0ef5c6cca4d381001f2f1
SafaaSaoudi <safa.saoudi@esprit.tn>
2025-09-16 11:39:13 +0100

    w2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Layout/header/header.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { HomeComponent } from './Layout/home/home.component';
+import { NotFoundComponent } from './Layout/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
